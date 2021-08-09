12a2de80587577147beba41f6f48ded053aa17a9
Jax <75493600+JaxRaffnix@users.noreply.github.com>
2021-08-09 23:37:05 +0200

    deleted low level files

--- gerade.py
+++
@@ -1,6 +0,0 @@
-x = 3
-
-if x % 2:
-    print("Ungerade")
-else:
-    print("Gerade")
--- modackermann.py
+++
@@ -1,8 +0,0 @@
-x = input("Erste Zahl x ist:")
-y = input("Zweite Zahl y ist:")
-
-def modack(k, n):
-    if n >= 1:
-        return 2 * n
-    else:
-        pass
